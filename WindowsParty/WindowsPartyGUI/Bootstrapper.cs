9a8a0ada86619073e9e67bb1453ee700602c224b
AurelijusV <aurelijus.vaitkevicius@gmail.com>
2020-04-26 14:31:14 +0300

    feat: Created initial project
    Added project structure. Added and configured Caliburn Micro.

---
+++ WindowsParty/WindowsPartyGUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using WindowsPartyGUI.ViewModels;
+using Caliburn.Micro;
+
+namespace WindowsPartyGUI
+{
+    public class Bootstrapper: BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
